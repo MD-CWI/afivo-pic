a9f9da2efa87b15480f706bbaee9656fa96deb04
Jannis Teunissen <jannis@teunissen.net>
2020-10-07 12:28:46 +0200

    Add example of how to include initial surface charge

--- programs/dielectric_2d/m_user.f90
+++ programs/dielectric_2d/m_user.f90
@@ -17,6 +17,8 @@ contains
     type(CFG_t), intent(inout) :: cfg
 
     user_initial_particles => init_particles
+    ! user_initial_ion_density => init_ion_density
+    user_set_surface_charge => init_surface_charge
     user_set_dielectric_eps => set_epsilon
     user_potential_bc => my_potential
   end subroutine user_initialize
@@ -34,12 +36,12 @@ contains
     part%t_left = 0.0_dp
 
     do n = 1, 1000
-       pos(1:2) = [0.26_dp, 0.9_dp] * domain_len
+       pos(1:2) = [0.25_dp, 0.5_dp] * domain_len
        pos(3)   = 0.0_dp
        part%w   = 1.0_dp
 
        rand_seed = GL_rng%two_normals()
-       part%x(1:2) = pos(1:2) + [rand_seed(1) * 2e-5_dp, rand_seed(2) * 1e-3_dp]
+       part%x(1:2) = pos(1:2) + [rand_seed(1) * 1e-3_dp, rand_seed(2) * 1e-3_dp]
        ! part%x(1:2) = pos(1:2) + GL_rng%two_normals() * 1e-5_dp
 
        if (outside_check(part) <= 0) then
@@ -47,6 +49,38 @@ contains
        end if
     end do
   end subroutine init_particles
+
+  real(dp) function init_surface_charge(r)
+    real(dp), intent(in) :: r(2)
+    real(dp)             :: z_dist
+    real(dp), parameter :: rel_z_pos = 0.5_dp
+    real(dp), parameter :: surface_density = 1.5e15_dp
+    real(dp), parameter :: decay_length = 4e-4_dp
+
+    z_dist = r(2) - rel_z_pos * domain_len(2)
+    init_surface_charge = surface_density * exp(-z_dist**2/decay_length**2)
+  end function init_surface_charge
+
+  subroutine init_ion_density(box)
+    use m_geometry
+    type(box_t), intent(inout) :: box
+    integer                    :: i, j, nc
+    real(dp)                   :: density, rr(2)
+
+    nc = box%n_cell
+
+    do j = 0, nc+1
+       do i = 0, nc+1
+          rr = af_r_cc(box, [i, j])
+          density = 2e18_dp * GM_density_line(rr, &
+               [0.26_dp, 0.95_dp] * domain_len, &
+               [0.26_dp, 0.9_dp] * domain_len, &
+               NDIM, 5e-5_dp, "smoothstep")
+          box%cc(i, j, i_pos_ion) = box%cc(i, j, i_pos_ion) + &
+               density
+       end do
+    end do
+  end subroutine init_ion_density
 
   subroutine set_epsilon(box)
     type(box_t), intent(inout) :: box
