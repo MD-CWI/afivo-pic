9aa00bffafd853f36e93ffe29e4cd69916bc84b7
Zhen Wang <zhenwang@int2.bullx>
2021-05-04 23:49:12 +0200

    Add parameters of initial seeds in 3d simulations

--- programs/standard_3d/m_user.f90
+++ programs/standard_3d/m_user.f90
@@ -6,6 +6,11 @@ module m_user
   implicit none
   private
 
+  real(dp)  :: seed_pos(3) = [0.5_dp, 0.5_dp, 0.5_dp]
+  integer   :: seed_num_particles = 10000
+  real(dp)  :: seed_particle_weight = 1e4
+  real(dp)  :: seed_sigma = 1e-4_dp
+
   ! Public methods
   public :: user_initialize
 
@@ -13,6 +18,15 @@ contains
 
   subroutine user_initialize(cfg)
     type(CFG_t), intent(inout) :: cfg
+
+    call CFG_add_get(cfg, "seed_pos", seed_pos, &
+         "relative position of initial seed")
+    call CFG_add_get(cfg, "seed_sigma", seed_sigma, &
+         "characteristic size of the initial seed")
+    call CFG_add_get(cfg, "seed_num_particles", seed_num_particles, &
+         "number of particles in the seed")
+    call CFG_add_get(cfg, "seed_particle_weight", seed_particle_weight, &
+         "weight of the particles in the seed")
 
     user_initial_particles => init_particles
   end subroutine user_initialize
@@ -30,11 +44,11 @@ contains
     part%a      = 0.0_dp
     part%t_left = 0.0_dp
 
-    do n = 1, 100
-       pos(1:3) = [0.5_dp, 0.5_dp, 0.75_dp] * domain_len
-       part%w   = 1.0_dp
-       part%x(1:2) = pos(1:2) + GL_rng%two_normals() * 1e-5_dp
-       part%x(2:3) = pos(2:3) + GL_rng%two_normals() * 1e-5_dp
+    do n = 1, seed_num_particles
+       pos(1:3) = seed_pos * domain_len
+       part%w   = seed_particle_weight
+       part%x(1:2) = pos(1:2) + GL_rng%two_normals() * seed_sigma
+       part%x(2:3) = pos(2:3) + GL_rng%two_normals() * seed_sigma
 
        if (outside_check(part) <= 0) then
           call pctest%add_part(part)
