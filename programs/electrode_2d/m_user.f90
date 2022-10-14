7cf406a5c42b3d385ea72690e7a2346b8eafe48f
lixiaoran1996 <lixiaoran1996@foxmail.com>
2022-10-14 16:37:37 +0800

    for discharges in pure CO2

--- programs/electrode_2d/m_user.f90
+++ programs/electrode_2d/m_user.f90
@@ -8,6 +8,12 @@ module m_user
   implicit none
   private
 
+  real(dp) :: seed_pos(2) = [0.5_dp, 0.5_dp]
+  real(dp) :: seed_sigma = 1e-4_dp
+  integer :: seed_num_particles = 10000
+  real(dp) :: seed_particle_weight = 1e4
+  logical  :: user_init_pc = .true.
+
   ! Public methods
   public :: user_initialize
 
@@ -16,49 +22,53 @@ contains
   subroutine user_initialize(cfg)
     type(CFG_t), intent(inout) :: cfg
 
-    user_initial_particles => init_particles
-    user_set_dielectric_eps => set_epsilon
+    call CFG_add_get(cfg, "pc%seed_pos", seed_pos, &
+         "relative position of initial seed")
+    call CFG_add_get(cfg, "pc%seed_sigma", seed_sigma, &
+         "characteristic size of the initial seed")
+    call CFG_add_get(cfg, "pc%seed_num_particles", seed_num_particles, &
+         "number of particles in the seed")
+    call CFG_add_get(cfg, "pc%seed_particle_weight", seed_particle_weight, &
+         "weight of the particles in the seed")
+    call CFG_add_get(cfg, "pc%user_init", user_init_pc, &
+         "use user_initial_particles or not")
+    if (user_init_pc) then
+      user_initial_particles => init_particles
+    end if
+    
   end subroutine user_initialize
 
   subroutine init_particles(pctest)
     use m_particle_core
     type(PC_t), intent(inout) :: pctest
     integer                   :: n
-    real(dp)                  :: pos(3)
+    real(dp)                  :: tmp_vec(2)
     type(PC_part_t)           :: part
 
     part%v      = 0.0_dp
     part%a      = 0.0_dp
     part%t_left = 0.0_dp
 
-    do n = 1, 1000
-       pos(1:2) = [0.5_dp, 0.75_dp] * domain_len
-       pos(3)   = 0.0_dp
-       part%w   = 1.0_dp
-       part%x(1:2) = pos(1:2) + GL_rng%two_normals() * 1e-3_dp
+    do n = 1, seed_num_particles
+       if (GL_cylindrical) then
+          tmp_vec = GL_rng%two_normals() * seed_sigma
+          part%x(1) = norm2(tmp_vec)
+          tmp_vec = GL_rng%two_normals() * seed_sigma
+          part%x(2) = tmp_vec(1)
+          part%x(1:2) = part%x(1:2) + seed_pos * domain_len
+          part%x(3) = 0.0_dp
+       else
+          part%x(1:2) = GL_rng%two_normals() * seed_sigma
+          part%x(1:2) = part%x(1:2) + seed_pos * domain_len
+          part%x(3) = 0.0_dp
+       end if
+
+       part%w = seed_particle_weight
 
        if (outside_check(part) <= 0) then
           call pctest%add_part(part)
        end if
     end do
   end subroutine init_particles
-
-  subroutine set_epsilon(box)
-    type(box_t), intent(inout) :: box
-    real(dp)                   :: r(2)
-    integer                    :: i, j
-
-    do j = 0, box%n_cell+1
-       do i = 0, box%n_cell+1
-          r = af_r_cc(box, [i, j])
-
-          if (r(1)/domain_len(1) < 0.125_dp) then
-             box%cc(i, j, i_eps) = 100.0_dp
-          else
-             box%cc(i, j, i_eps) = 1.0_dp
-          end if
-       end do
-    end do
-  end subroutine set_epsilon
 
 end module m_user
